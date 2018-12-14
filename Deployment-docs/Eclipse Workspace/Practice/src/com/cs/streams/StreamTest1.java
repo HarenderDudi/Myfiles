925fac6a75f3e154ff44b7459efa8a371b54dcf5
unknown <Harender.Dudi@contentserv.com>
2018-12-14 09:52:15 +0530

    Commit: 14th Dec

---
+++ Deployment-docs/Eclipse Workspace/Practice/src/com/cs/streams/StreamTest1.java
@@ -0,0 +1,17 @@
+package com.cs.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+
+public class StreamTest1 {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+	
+	}
+
+}
