ba5a1455114b6e5a1410e6f03597b34f490102b9
harende barclays <harendedudi1991@gmail.com>
2025-03-20 00:27:51 +0530

    20 march 2025 count anagram occurrence sliding window

--- Java DS Practice/src/main/java/com/dudi/array/string/slidingwindow/CountOccurrencesAnagrams.java
+++ Java DS Practice/src/main/java/com/dudi/array/string/slidingwindow/CountOccurrencesAnagrams.java
@@ -1,5 +1,9 @@
 package com.dudi.array.string.slidingwindow;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class CountOccurrencesAnagrams {
 
 	public static void main(String[] args) {
@@ -11,7 +15,47 @@ public class CountOccurrencesAnagrams {
 		// use sliding window
 		// if(j-i+1 < k) -> get char from map and reduce its count, also check if its count is 0 then count--
 		// if(j-i+1 == k) check if count is 0, yes then result++, since we are going to increase i++ we need to increment count for the arr[i] in map
-		
+
+		String pattern = "abba";
+		char[] input = "acababaabcd".toCharArray();
+
+		Map<Character, Long> freqMap = pattern.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		int windowSize = pattern.length();
+
+		int i=0;
+		int j=0;
+
+		int count = freqMap.size();
+		int ans = 0;
+
+		while(j < input.length){
+			if(freqMap.containsKey(input[j])){
+				freqMap.put(input[j], freqMap.get(input[j])-1);
+
+				if(freqMap.get(input[j]) == 0 && count>0){
+					count--;
+				}
+			}
+
+			if(j-i+1 < windowSize)
+				j++;
+			else {
+				if(count==0){
+					ans++;
+				}
+
+				if(freqMap.containsKey(input[i])){
+					freqMap.put(input[i], freqMap.get(input[i])+1);
+					if(freqMap.get(input[i]) > 0){
+						count++;
+					}
+				}
+				i++;
+				j++;
+			}
+		}
+
+		System.out.println(ans);
 	}
 
 }
